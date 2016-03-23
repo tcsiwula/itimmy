ff68ba02a07c78d391b9e15467e85056a918afff
tcsiwula <tcsiwula@usfca.edu>
2016-03-23 02:55:17 -0700

    wow

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var express = require('express')
+var serveIndex = require('serve-index');
+
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('itimmy.com is under constructions. Migrating to iojs 😀')
-  document.body.style.backgroundColor = 'red'
-  response.log("Node app is running at localhost:" + app.get('port'))
-})
+app.use(serveIndex(__dirname + '/public'))
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+    console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+// app.get('/', function(request, response) {
+//     response.send('itimmy.com is under constructions. Migrating to iojs 😀')
+//     document.body.style.backgroundColor = 'red'
+//     response.log("Node app is running at localhost:" + app.get('port'))
+// })
