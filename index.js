f9cf2200635500e4262769bb5d0352889c3b05e9
tcsiwula <tcsiwula@usfca.edu>
2016-03-26 20:48:17 -0700

    added /about and /resume

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var app = express();
 var pgp = require('pg-promise')({
     // Initialization Options
 });
-
+var date = new Date();
+var current_hour = date.getHours();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 app.use(serveIndex(__dirname + '/public'))
@@ -14,18 +15,32 @@ app.use(serveIndex(__dirname + '/public'))
 
 
 app.get('/', function(req,res){
-    res.send("Hello from /");
+    res.send("Hello from /. The current time is: ", current_hour);
 })
 
 
 
 app.get('/about', function(req,res){
-    res.send("Hello from /about");
+    res.send("Hello from /about. The current time is: ", current_hour);
 })
 
 app.get('/resume', function(req,res){
-    res.send("Hello from /resume");
+    res.send("Hello from /redume. The current time is: ", current_hour);
 })
+
+
+
+
+
+
+
+
+// var options = {
+//     key: fs.readFileSync('./example/localhost.key'),
+//     cert: fs.readFileSync('./example/localhost.crt')
+// };
+//
+// require('http2').createServer(options, app).listen(8080);
 
 
 
@@ -33,6 +48,8 @@ app.get('/resume', function(req,res){
 app.listen(process.env.PORT || 5000, function()
 {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log("date = ", date);
+    console.log("current_hour = ", current_hour);
     open('http://127.0.0.1:5000');
 });
 
