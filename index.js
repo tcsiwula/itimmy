679d3937ecc346d6b166fcf9194c757d25b09df7
Tim Siwula <tcsiwula@usfca.edu>
2016-11-21 22:26:24 -0800

    added libasound2-dev to Aptfile

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var pgp = require('pg-promise')({
 
 var date = new Date();
 var current_hour = date.getHours();
-app.set('port', (process.env.PORT || 5000))
+//app.set('port', (process.env.PORT || 5000))
 // app.use(express.static(__dirname + '/public'))
 app.use(serveIndex('/public'));
 //app.use(serveIndex(__dirname + '/revive'))
