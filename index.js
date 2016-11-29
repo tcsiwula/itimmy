e96b26b261b6cd0a47a3dc08ebc7ccc9c9801d24
Tim Siwula <tcsiwula@usfca.edu>
2016-11-28 18:13:44 -0600

    threejs landing page

--- index.js
+++ index.js
@@ -4,19 +4,12 @@ var open = require('open');
 var app = express();
 var fs = require('fs');
 var path = require('path');
-//var lame = require('lame');
-//var Speaker = require('speaker');
 var pgp = require('pg-promise')({
-    // Initialization Options
 });
 
 var date = new Date();
 var current_hour = date.getHours();
-//app.set('port', (process.env.PORT || 5000))
-// app.use(express.static(__dirname + '/public'))
 app.use(serveIndex('/public'));
-//app.use(serveIndex(__dirname + '/revive'))
-//app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
@@ -86,13 +79,6 @@ app.get('/c', function (req, res) {
     res.writeHead(301, {"Location": "https://twitter.com/tcsiwula"});
     res.end();
 })
-
-// play mp3 file
-// fs.createReadStream("public/audio/erikSatieNumberThree.mp3")
-//     .pipe(new lame.Decoder())
-//     .on('format', function (format) {
-//         this.pipe(new Speaker(format));
-//     });
 
 app.listen(process.env.PORT || 5000, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
