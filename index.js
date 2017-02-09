59b2846f6137501a3e6398bd717ec2afac4430c0
Tim Siwula <tcsiwula@gmail.com>
2017-02-09 01:23:45 -0800

    redirect to heroku for chatbot

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var open = require('open');
 var app = express();
 var fs = require('fs');
 var path = require('path');
-
 const bodyParser = require('body-parser');
 const request = require('request');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 var date = new Date();
@@ -15,7 +15,6 @@ var current_hour = date.getHours();
 app.use(serveIndex('/public'));
 app.use(express.static('public'));
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const APIAI_TOKEN = process.env.APIAI_TOKEN;
 
 const server = app.listen(process.env.PORT || 5000, () => {
     console.log("date = ", date);
@@ -72,27 +71,14 @@ app.get('/about', function (req, res) {
 // app https://developers.facebook.com/apps/1342241162503801/messenger/
 /* For Facebook Validation */
 app.get('/messenger_chatbot', (req, res) => {
-    if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'tuxedo_cat') {
-        res.status(200).send(req.query['hub.challenge']);
-    } else {
-        res.send("Hello from 403 status.");
-        res.status(403).end();
-    }
+    res.writeHead(301, {"Location": "https://rocky-journey-79030.herokuapp.com/messenger_chatbot/"});
+    res.end();
 });
 
 /* Handling all messenges */
 app.post('/messenger_chatbot', (req, res) => {
-    console.log(req.body);
-    if (req.body.object === 'page') {
-        req.body.entry.forEach((entry) => {
-            entry.messaging.forEach((event) => {
-                if (event.message && event.message.text) {
-                    sendMessage(event);
-                }
-            });
-        });
-        res.status(200).end();
-    }
+    res.writeHead(301, {"Location": "https://rocky-journey-79030.herokuapp.com/messenger_chatbot/"});
+    res.end();
 });
 
 function sendMessage(event) {
